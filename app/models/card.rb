ccfbf40399d8c0dddb3350896d19c4822079e500
Phil Gengler <phil@pgengler.net>
2012-12-10 16:18:32 -0500

    Require front/back content for cards

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,7 @@
 class Card < ActiveRecord::Base
 
+	validates_presence_of :front, :back
+
 	def self.random
 		rand_id = rand(Card.count)
 		Card.first :conditions => [ 'id >= ?', rand_id ]
--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "requires a front" do
+		assert_raises ActiveRecord::RecordInvalid do
+			Card.create! :back => 'back'
+		end
+	end
+
+	test "requires a back" do
+		assert_raises ActiveRecord::RecordInvalid do
+			Card.create! :front => 'front'
+		end
+	end
 end
