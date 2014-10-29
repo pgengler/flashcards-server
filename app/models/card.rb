cc06eb707ff748d1100c2021eeb494dd3b07610e
Phil Gengler <phil@pgengler.net>
2014-10-28 23:12:58 -0400

    Fix random card selection

--- app/models/card.rb
+++ app/models/card.rb
@@ -3,8 +3,7 @@ class Card < ActiveRecord::Base
 	validates_presence_of :front, :back
 
 	def self.random
-		rand_id = rand(Card.count)
-		Card.first :conditions => [ 'id >= ?', rand_id ]
+		Card.limit(1).order("RANDOM()").first
 	end
 
 end
