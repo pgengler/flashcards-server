89781c30fd0eae02763ff1cc20919f9eddd4d777
Phil Gengler <phil@pgengler.net>
2014-11-11 12:28:13 -0500

    Add ability to get random card front (previous behavior), random card back, or random front or back

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,53 +1,71 @@
 class CardsController < ApplicationController
-  def random
-    @card = Card.random
-    if @card
-      render :show
-    else
-      redirect_to new_card_path
-    end
-  end
+	before_filter :find_random_card_or_redirect, only: [ :random, :random_front, :random_back ]
+	def random
+		if rand() < 0.5
+			random_front
+		else
+			random_back
+		end
+	end
 
-  def show
-    @card = Card.find(params[:id])
-  end
+	def random_front
+		render :show
+	end
 
-  def new
-    @card = Card.new
-    @form_title = 'New card'
-  end
+	def random_back
+		tmp = @card.front
+		@card.front = @card.back
+		@card.back = tmp
+		render :show
+	end
 
-  def create
-    @card = Card.create!(card_params)
+	def show
+		@card = Card.find(params[:id])
+	end
 
-    flash[:notice] = 'Card added'
-    redirect_to card_path(@card)
-  end
+	def new
+		@card = Card.new
+		@form_title = 'New card'
+	end
 
-  def edit
-    @card = Card.find(params[:id])
-    @form_title = 'Edit card'
-  end
+	def create
+		@card = Card.create!(card_params)
 
-  def update
-    @card = Card.find(params[:id])
-    @card.update_attributes card_params
+		flash[:notice] = 'Card added'
+		redirect_to card_path(@card)
+	end
 
-    flash[:notice] = 'Card saved'
-    redirect_to card_path(@card)
-  end
+	def edit
+		@card = Card.find(params[:id])
+		@form_title = 'Edit card'
+	end
 
-  def destroy
-    @card = Card.find(params[:id])
-    @card.destroy
+	def update
+		@card = Card.find(params[:id])
+		@card.update_attributes card_params
 
-    flash[:notice] = 'Card deleted'
-    redirect_to cards_path
-  end
+		flash[:notice] = 'Card saved'
+		redirect_to card_path(@card)
+	end
+
+	def destroy
+		@card = Card.find(params[:id])
+		@card.destroy
+
+		flash[:notice] = 'Card deleted'
+		redirect_to cards_path
+	end
 
 	private
 
 	def card_params
 		params.require(:card).permit(:front, :back)
+	end
+
+	def find_random_card_or_redirect
+		@card = Card.random
+		unless @card
+			redirect_to new_card_path
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	get 'cards/random' => 'cards#random', as: :random_card
+	get 'cards/random/front' => 'cards#random_front', as: :random_card_front
+	get 'cards/random/back' => 'cards#random_back', as: :random_card_back
 	resources :cards, except: [ :index ]
 	root to: 'cards#random'
 end
