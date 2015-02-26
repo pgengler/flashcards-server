64d011c9223fc540b3b7847df49c0836ff6bf8ea
Phil Gengler <phil@pgengler.net>
2015-02-26 14:10:26 -0500

    Update and simplify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,97 +1,20 @@
-require 'bundler/capistrano'
+# config valid only for current version of Capistrano
+lock '3.3.5'
 
-# This capistrano deployment recipe is made to work with the optional
-# StackScript provided to all Rails Rumble teams in their Linode dashboard.
-#
-# After setting up your Linode with the provided StackScript, configuring
-# your Rails app to use your GitHub repository, and copying your deploy
-# key from your server's ~/.ssh/github-deploy-key.pub to your GitHub
-# repository's Admin / Deploy Keys section, you can configure your Rails
-# app to use this deployment recipe by doing the following:
-#
-# 1. Add `gem 'capistrano'` to your Gemfile.
-# 2. Run `bundle install --binstubs --path=vendor/bundles`.
-# 3. Run `bin/capify .` in your app's root directory.
-# 4. Replace your new config/deploy.rb with this file's contents.
-# 5. Configure the two parameters in the Configuration section below.
-# 6. Run `git commit -a -m "Configured capistrano deployments."`.
-# 7. Run `git push origin master`.
-# 8. Run `bin/cap deploy:setup`.
-# 9. Run `bin/cap deploy:migrations` or `bin/cap deploy`.
-#
-# Note: When deploying, you'll be asked to enter your server's root
-# password. To configure password-less deployments, see below.
+set :application, 'flashcards-server'
+set :repo_url, 'https://github.com/pgengler/flashcards-server.git'
 
-#############################################
-##                                         ##
-##              Configuration              ##
-##                                         ##
-#############################################
+set :deploy_to, '/srv/apps/flashcards/server'
 
-GITHUB_REPOSITORY_NAME = 'pgengler/flashcards'     # <username>/<repo>
-LINODE_SERVER_HOSTNAME = 'example.com'
+set :linked_files, %w{ .env }
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'node_modules', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-#############################################
-#############################################
+namespace :deploy do
+  after :publishing, :restart
 
-# General Options
-
-set :bundle_flags,               "--deployment"
-
-set :application,                "flashcards"
-set :deploy_to,                  "/srv/apps/flashcard"
-set :normalize_asset_timestamps, false
-set :rails_env,                  "production"
-
-set :user,                       "root"
-set :runner,                     "www-data"
-set :admin_runner,               "www-data"
-
-# Password-less Deploys (Optional)
-#
-# 1. Locate your local public SSH key file. (Usually ~/.ssh/id_rsa.pub)
-# 2. Execute the following locally: (You'll need your Linode server's root password.)
-#
-#    cat ~/.ssh/id_rsa.pub | ssh root@LINODE_SERVER_HOSTNAME "cat >> ~/.ssh/authorized_keys"
-#
-# 3. Uncomment the below ssh_options[:keys] line in this file.
-#
-# ssh_options[:keys] = ["~/.ssh/id_rsa"]
-
-# SCM Options
-set :scm,        :git
-set :repository, "git://github.com/#{GITHUB_REPOSITORY_NAME}.git"
-set :branch,     "master"
-
-# Roles
-role :app, LINODE_SERVER_HOSTNAME
-role :db,  LINODE_SERVER_HOSTNAME, :primary => true
-role :web, LINODE_SERVER_HOSTNAME
-
-# Add Configuration Files & Compile Assets
-after 'deploy:update_code' do
-  # Setup Configuration
-  run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-
-  # Compile Assets
-  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+  task :restart do
+    on roles(:app) do
+      invoke 'puma:restart'
+    end
+  end
 end
-
-# Restart Passenger
-deploy.task :restart, :roles => :app do
-  # Fix Permissions
-  sudo "chown -R www-data:www-data #{current_path}"
-  sudo "chown -R www-data:www-data #{latest_release}"
-  sudo "chown -R www-data:www-data #{shared_path}/bundle"
-  sudo "chown -R www-data:www-data #{shared_path}/log"
-
-  # Restart Application
-  run "touch #{current_path}/tmp/restart.txt"
-end
-
-namespace :db do
-	task :seed do
-		run "cd #{current_path}; RAILS_ENV=production bundle exec rake db:seed"
-	end
-end
-        require './config/boot'
