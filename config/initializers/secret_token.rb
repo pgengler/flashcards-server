7d132e12d264832f0b83137ee65da6b2b23ddd33
Phil Gengler <phil@pgengler.net>
2015-03-10 14:21:34 -0400

    Copy config files from a new Rails 4.2 app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,18 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Flashcards::Application.config.secret_token = '4b3cf2a5f53cb3d5325fc18c73d43698c69161c175ac9cb9cf65941ed02cf3cf06c7b9bbe4b1d00dc57ca89777db804040ce50ad3c2c76648589fd865d201254'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Flashcards::Application.config.secret_token = 'ce49648e4833905acd6a097f1f05e1a0df4ee78dffa47ea7c4ecd24cae193084484d36b7a2794837a13f0362481ffa7fe214645c23f4e2fa78bba888c65a98c2'
