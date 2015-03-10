1c4755c699562fa5c7c690dcb1e9cd876082b8b4
Phil Gengler <phil@pgengler.net>
2015-03-10 12:44:55 -0400

    Use Ruby 1.9 hash syntax

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class CardTest < ActiveSupport::TestCase
 	test "requires a front" do
 		assert_raises ActiveRecord::RecordInvalid do
-			Card.create! :back => 'back'
+			Card.create! back: 'back'
 		end
 	end
 
 	test "requires a back" do
 		assert_raises ActiveRecord::RecordInvalid do
-			Card.create! :front => 'front'
+			Card.create! front: 'front'
 		end
 	end
 end
