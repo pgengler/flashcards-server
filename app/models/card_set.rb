ddacb6e28020695255e118bd3826c58f6dd38b2f
Phil Gengler <phil@pgengler.net>
2021-07-18 07:48:59 -0400

    Card set names should be unique within a collection but not across collections

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -2,6 +2,6 @@ class CardSet < ApplicationRecord
 	has_and_belongs_to_many :cards
 	belongs_to :collection
 
-	validates :name, presence: true, uniqueness: true
+	validates :name, presence: true, uniqueness: { scope: :collection }
 	validates :collection, presence: true
 end
--- test/models/card_set_test.rb
+++ test/models/card_set_test.rb
@@ -1,17 +1,28 @@
 class CardSetTest < ActiveSupport::TestCase
-	test "name is required" do
+	test 'name is required' do
 		collection = create(:collection)
 		assert_raises ActiveRecord::RecordInvalid do
 			CardSet.create! collection: collection
 		end
 	end
 
-	test "name must be unique" do
-		card_set = create(:card_set)
+	test 'name must be unique within a collection' do
 		collection = create(:collection)
+		card_set = create(:card_set, collection: collection)
 		assert_raises ActiveRecord::RecordInvalid do
 			CardSet.create! name: card_set.name, collection: collection
 		end
+	end
+
+	test 'names can be duplicated across collections' do
+		name = 'A Card Set'
+		create :card_set, name: name, collection: create(:collection)
+		collection = create(:collection)
+
+		assert_nothing_raised do
+			CardSet.create! name: name, collection: collection
+		end
+
 	end
 
 	test 'requires a collection' do
